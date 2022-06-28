1c21f5c131571127934321f38ab1506bf29065b7
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-06-28 16:51:28 +0000

    修改 搜尋關鍵字功能

--- search.py
+++ search.py
@@ -31,13 +31,11 @@ for _ in range(1,11):
 date.reverse()
 name.reverse()
 chat.reverse()
-for z in range(len(chat)):
-    count += 1
-    if search in chat[z]:
-        sum += 1
-        print("代號",sum,"第"+str(count)+"則訊息\n"+name[z],date[z]+"\n\n"+chat[z]+"\n")
-        print('------------------------')
-print("總共搜尋到",sum,"結果")
 
-view = int(input("要檢視的代號?:"))
-history = int(input("要檢視前後幾則訊息"))
+mode = int(input("請選擇查詢模式\n1.顯示完整聊天紀錄\n2.顯示某一天的聊天紀錄\n3.搜尋關鍵字訊息\n"))
+
+if mode == 3:
+    search = input("輸入想搜尋的關鍵字:")
+    print()
+    print('------------------------')
+    search_chat(search)
